04c8ac3547dae5a9e0287c09f41203e317584031
CoralMartinez <coralmtzs084@gmail.com>
2026-06-18 19:20:43 -0600

    practica_9_REACT NATIVE_  ActivityIndicator y KeyboardAvoidingView
    Finalizada

---
+++ Practica_5/miapp2/screens/ActivityIndicator_KeyboardAvoidingView.js
@@ -0,0 +1,181 @@
+import React, {useState} from 'react';
+import {StatusBar} from 'expo-status-bar';
+
+import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform, TouchableOpacity, 
+  ActivityIndicator, Alert, View} from 'react-native';
+
+
+export default function ActivityIndicator_KeyboardAvoidingView(){
+
+  const[nombre, setNombre] = useState('');
+  const[correo, setCorreo] = useState('');
+  const[contrasena, setContrasena] = useState('');
+
+  const[cargando, setCargando] = useState(false);
+  const[mensaje, setMensaje] = useState('');
+
+  const iniciarSesion = () =>{
+
+    if(nombre ===''|| correo === '' || contrasena===''){
+      Alert.alert('Campos vacíos', 'Complete todos los campos');
+      return;
+    }
+
+    setMensaje('');
+    setCargando(true);
+
+    setTimeout(() => {
+
+      setCargando(false);
+
+      setMensaje('Datos válidos. Bienvenido(a)'+nombre);
+
+    }, 3000);
+
+  };
+
+
+  return(
+
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding':'height'}>
+    
+      <Text style={styles.titulo}>
+        ActivityIndicator y KeyboardAvoidingView
+
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Correo"
+        keyboardType="email-address"
+        value={correo}
+        onChangeText={setCorreo}
+
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        value={contrasena}
+        onChangeText={setContrasena}      
+      />
+
+      {
+        cargando ?(
+
+          <View style={styles.spinnerContainer}>
+
+            <ActivityIndicator
+              size="large"
+              color="blue"
+              animating={cargando}
+            />
+
+            <Text>
+              Validando información...
+            </Text>
+
+          </View>
+        ) : (
+
+          <TouchableOpacity
+            style={styles.boton}
+            onPress={iniciarSesion}
+          >
+
+          <Text style={styles.textoBoton}>
+            Iniciar Sesión
+          </Text>
+
+          </TouchableOpacity>
+
+        )
+      }
+
+      <Text style={styles.mensaje}>
+        {mensaje}
+      </Text>
+
+      <StatusBar style="auto"/>
+    
+    </KeyboardAvoidingView>
+
+
+
+  );
+
+
+}
+
+
+ const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor:'#ffffff',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: 20
+  },
+
+  titulo:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:20,
+    textAlign:'center'
+  },
+
+  input:{
+    width:'90%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius:8, 
+    paddingHorizontal:10,
+    marginVertical:5
+  },
+
+  boton:{
+    backgroundColor: '#2196F3',
+    width: '90%',
+    padding: 15,
+    borderRadius:8,
+    marginTop:20
+  },
+
+  textoBoton:{
+    color:'#ffffff',
+    textAlign: 'center',
+    fontWeight: 'bold'
+
+  },
+
+  spinnerContainer:{
+    alignItems: 'center',
+    marginTop:20
+  },
+
+  mensaje:{
+
+    marginTop:20,
+    fontSize:18,
+    fontWeight:'bold',
+    color: 'green',
+    textAlign: 'center'
+
+  }
+
+ });
+
+
+
