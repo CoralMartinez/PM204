4dad7224f181a2ed37acde820914bf7b2515b2c4
CoralMartinez <coralmtzs084@gmail.com>
2026-06-29 22:21:48 -0600

    Avances_Practica_15_Uno_ImageBackgroung & SlapshScreen:

--- Practica_5/miapp2/screens/Uno_ImageBackground_SlapshScreen.js
+++ Practica_5/miapp2/screens/Uno_ImageBackground_SlapshScreen.js
@@ -1,22 +1,31 @@
 /*Zona 1: Importaciones, componentes y archivos*/
 
+import {useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, ImageBackground, Image} from 'react-native';
 
 
 /*Zona 2: Main - Renderizado de componentes */
 export default function Uno_ImageBackground_SlapshScreen() {
+
+  const {imagenindex, setImagen}= useState(0);
+  const {blur, setblur} = useState(0);
+
+  const imagenes = [
+    requiere('..assets/icon.png'),
+    require('..assets/UPQ-Logo.png'),
+    require('..assets/image.png'),
+  ];
+
   return (
-    <View style={styles.container}>
+    <ImageBackground source={imagenes[imagenindex]} 
+      style={styles.contenedor} 
+      imageStyle={styles.imagen}
+      blurRadius={blur}
+    />
 
-        <Text>Aquí va la 1° práctica de ImageBackground y SlapshScreen</Text>
-
-
-      <StatusBar style="auto" />
     
-
-    </View>
-  );  
+  );   
   
 }
 
