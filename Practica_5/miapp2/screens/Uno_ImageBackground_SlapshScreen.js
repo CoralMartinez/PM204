25a301f942c1dbae5d1c71f998153cb0634749a1
CoralMartinez <coralmtzs084@gmail.com>
2026-06-30 13:46:15 -0600

    cambios

--- Practica_5/miapp2/screens/Uno_ImageBackground_SlapshScreen.js
+++ Practica_5/miapp2/screens/Uno_ImageBackground_SlapshScreen.js
@@ -1,11 +1,10 @@
-/*Zona 1: Importaciones, componentes y archivos*/
+/*
 
 import {useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View, Button, ImageBackground, Image} from 'react-native';
 
 
-/*Zona 2: Main - Renderizado de componentes */
 export default function Uno_ImageBackground_SlapshScreen() {
 
   const {imagenindex, setImagen}= useState(0);
@@ -29,7 +28,7 @@ export default function Uno_ImageBackground_SlapshScreen() {
   
 }
 
-/*Zona 3: Estilos y posicionamiento */
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -42,5 +41,6 @@ const styles = StyleSheet.create({
 
 });
 
+*/
 
 
